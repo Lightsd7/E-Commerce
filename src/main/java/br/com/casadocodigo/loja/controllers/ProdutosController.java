90c9dfec8b6755c5b2d48063274d680246d9cc01
Raphael Molina <raphael.bedran@konduto.com>
2020-08-18 20:15:04 -0300

    Always Redirect After Post
    Adicionando redirect no metodo grava().
    Implementação de Flash Attribute.

--- src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProdutosController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.casadocodigo.loja.daos.ProdutoDAO;
 import br.com.casadocodigo.loja.models.Produto;
@@ -27,13 +28,14 @@ public class ProdutosController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public String grava(Produto produto) {
+	public ModelAndView grava(Produto produto, RedirectAttributes redirectAttributes) {
 		System.out.println(produto);
 		produtoDAO.gravar(produto);
-		return "produtos/ok";
+		redirectAttributes.addFlashAttribute("sucesso", "Produto cadastrado com sucesso!");
+		return new ModelAndView("redirect:produtos");
 	}
 
-	@RequestMapping( method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView listar() {
 		List<Produto> produtos = produtoDAO.listar();
 		ModelAndView modelAndView = new ModelAndView("produtos/lista");
